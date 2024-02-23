67a4040e996af716b0229a0be48e80dd723e6fe4
Julio Jurado <julioejurado@gmail.com>
2024-02-23 07:11:59 -0600

    Add Montserrat_Alternates font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { titleFont } from './config/fonts';
 
 export default function Home() {
   return (
     <main className="">
      <h1>Hola Universo</h1>
+     <h1 className={ `${titleFont.className} font-bold` } > Hola Space</h1>
     </main>
   );
 }
